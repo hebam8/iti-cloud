032c0cd2cd50ba6bc52f2b65b0f73e4e63c9faf5
unknown <hebam1017@gmail.com>
2022-01-28 19:55:34 +0200

    final commit

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello ITI");
+});
 
-app.get('/',(req,res)=>{
-  res.send("Hello World")
-})
-
-app.listen(process.env.PORT || 6000,()=>{
-    console.log("Listening On 6000")
-})
+app.listen(process.env.PORT || 6000, () => {
+  console.log("Listening On 6000");
+});
